20a5ede0ae45e9a82d46d8d8b9a005e6205d0112
r1ft4468 <pennoser@gmail.com>
2016-11-19 10:54:01 -0600

    1.1

--- addonbackup.bat
+++ addonbackup.bat
@@ -1,7 +1,5 @@
 @ECHO off
-set SAVE_TO="%userprofile%\Google Drive\Projects\WoWAddons\"
-
-cls
+CLS
 :init
 setlocal DisableDelayedExpansion
 set "batchPath=%~0"
@@ -16,12 +14,12 @@ if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
 :getPrivileges
 if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
 
-echo Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
-echo args = "ELEV " >> "%vbsGetPrivileges%"
-echo For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
-echo args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
-echo Next >> "%vbsGetPrivileges%"
-echo UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+ECHO args = "ELEV " >> "%vbsGetPrivileges%"
+ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+ECHO Next >> "%vbsGetPrivileges%"
+ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
 "%SystemRoot%\System32\WScript.exe" "%vbsGetPrivileges%" %*
 exit /B
 
@@ -30,27 +28,28 @@ setlocal & pushd .
 cd /d %~dp0
 if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
 
-echo Ready to Backup Addons and Settings for World of Warcraft
-echo Backing up to %SAVE_TO%
-pause
-if exist %temp%\wow_install\ rmdir /S /Q %temp%\wow_install\
+SET SAVE_TO="%userprofile%\Google Drive\Projects\WoWAddons\"
+ECHO Ready to Backup Addons and Settings for World of Warcraft
+ECHO Backing up to %SAVE_TO%
+PAUSE
+IF EXIST %temp%\wow_install\ rmdir /S /Q %temp%\wow_install\
 mkdir %temp%\wow_install\
-xcopy /Q /E ".\addonbackup" %temp%\wow_install\
-mklink /D %temp%\wow_install\Bin\WTF "%PROGRAMFILES(X86)%\World of Warcraft\WTF"
-mklink /D %temp%\wow_install\Bin\Interface "%PROGRAMFILES(X86)%\World of Warcraft\Interface"
-cd %temp%\wow_install\
+XCOPY /Q /E ".\addonbackup" %temp%\wow_install\
+MKLINK /D %temp%\wow_install\Bin\WTF "%PROGRAMFILES(X86)%\World of Warcraft\WTF"
+MKLINK /D %temp%\wow_install\Bin\Interface "%PROGRAMFILES(X86)%\World of Warcraft\Interface"
+CD %temp%\wow_install\
 upx --ultra-brute 7zsd.sfx 
 cd Bin
 ..\7za a -mx=9 "..\Program.7z" * 
 cd ..
 copy /b 7zsd.sfx + Config.txt + Program.7z WoW_Settings.exe
 del Program.7z
-move %temp%\wow_install\WoW_Settings.exe %SAVE_TO%
-cd ..
-rmdir /S /Q wow_install\
+MOVE %temp%\wow_install\WoW_Settings.exe %SAVE_TO%
+CD ..
+RMDIR /S /Q wow_install\
 cd %SAVE_TO%
 git add WoW_Settings.exe
 git commit -a -m Update
 git push
-echo Done Backing WoW Settings to %SAVE_TO%
-pause
+ECHO Done Backing WoW Settings to %SAVE_TO%
+PAUSE
--- addonbackup/Bin/wow_addon.bat
+++ addonbackup/Bin/wow_addon.bat
@@ -1,13 +1,13 @@
 @echo off
 IF EXIST "%PROGRAMFILES(X86)%\World of Warcraft\Wow.exe" (
 	SET WOWDIR="%PROGRAMFILES(X86)%\World of Warcraft\" 
-	SET INTERFACEDIR="%PROGRAMFILES(X86)%World of Warcraft\interface\"
-	SET WTFDIR="C:\Program Files (x86)\World of Warcraft\WTF\"
+	SET INTERFACEDIR="%PROGRAMFILES(X86)%\World of Warcraft\Interface\"
+	SET WTFDIR="%PROGRAMFILES(X86)%\World of Warcraft\WTF\"
 	GOTO :OPT
 	) ELSE (
 	IF EXIST "%PROGRAMFILES%\World of Warcraft\Wow.exe" (
 		SET WOWDIR="%PROGRAMFILES%\World of Warcraft\" 
-		SET INTERFACEDIR="%PROGRAMFILES%\World of Warcraft\interface\"
+		SET INTERFACEDIR="%PROGRAMFILES%\World of Warcraft\Interface\"
 		SET WTFDIR="%PROGRAMFILES%\World of Warcraft\WTF\"
 		GOTO :OPT
 		)
@@ -17,5 +17,5 @@ GOTO :EOF
 :OPT
 RMDIR /S /Q %INTERFACEDIR%
 RMDIR /S /Q %WTFDIR%
-XCOPY /E interface %INTERFACEDIR%
+XCOPY /E Interface %INTERFACEDIR%
 XCOPY /E WTF %WTFDIR%
